2d987280155a7e9b6e84c188b82c8815e18e264b
Karthik Palaniappan <karth295@cs.uw.edu>
2014-04-27 21:02:29 -0700

    Started js and html for login

--- app/assets/javascripts/users_controller.js
+++ app/assets/javascripts/users_controller.js
@@ -1,3 +1,16 @@
 function UsersController($scope) {
 	$scope.title = "Hello, world";
+  $scope.login = function() {
+    var data = {}
+    //TODO
+    $.ajax({
+      type: "GET",
+      url: //TODO,
+      dataType: "JSON",
+      success: function(data) {
+        console.log(data);
+        //TODO
+      }
+    });
+  }
 }
