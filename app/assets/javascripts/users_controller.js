53d862463b52e1d7e2376169b5aab6745a8647a2
Karthik Palaniappan <karth295@cs.uw.edu>
2014-04-28 17:33:41 -0700

    Minor changes, adding angular to login.html.erb

--- app/assets/javascripts/users_controller.js
+++ app/assets/javascripts/users_controller.js
@@ -1,16 +1,29 @@
 function UsersController($scope) {
-	$scope.title = "Hello, world";
-  $scope.login = function() {
-    var data = {}
+  $scope.login = function(e) {
+    if(!$scope.login_uname || !$scope.login_psswd) {
+      //TODO
+      return;
+    }
+
+    var data = {
+      uname: $scope.login_uname,
+      password: $scope.login_psswd
+    }
+    console.log(data);
+
     //TODO
     $.ajax({
-      type: "GET",
-      url: //TODO,
+      type: "POST",
+      url: "http://localhost:3000/createuser",
       dataType: "JSON",
       success: function(data) {
         console.log(data);
         //TODO
       }
     });
+  }
+
+  $scope.create_account = function(e) {
+    if(!$scope.create_
   }
 }
