0f1435aa3554aacdf648ebaf19c34fbc987ee433
Karthik Palaniappan <karth295@cs.uw.edu>
2014-04-30 00:27:34 -0700

    Started work on model and controller logic with dummy objects

--- devfiles/models-and-controllers/eventmodel.js
+++ devfiles/models-and-controllers/eventmodel.js
@@ -8,18 +8,25 @@ var Event = function() {
 	this.dateDue = null;
 	this.cycle = false;
 	this.repost = [false, false, false, false, false, false, false];
-
-	this.createEvent = function() {
-		//TODO
-	}
-
-	this.editEvent = function(id) {
-		//TODO
-	}
 }
 
 angular.module("myapp").factory('EventModel', function() {
-  var EventModel = new Event();
+  var EventModel = {};
+  EventModel.events = [];
+	EventModel.createEvent = function() {
+		//TODO
+	};
+	
+	EventModel.editEvent = function(id) {
+		//TODO
+	}
+
+	EventModel.getEvents = function() {
+		var eventsCopy = [];
+		for(event in events) {
+			
+		}
+	}
 
   return EventModel;
 });
--- devfiles/models-and-controllers/usermodel.js
+++ devfiles/models-and-controllers/usermodel.js
@@ -1,12 +1,38 @@
+var User = {
+	uname: "",
+	fname: "",
+	lname: ""
+};
+
 angular.module("myapp").factory('UserModel', function() {
   var UserModel = {}
-  UserModel.id = 0;
-  UserModel.uname = "";
-  UserModel.fname = "";
-  UserModel.lname = "";
+  var me = new User();
+  
 
   UserModel.login = function(uname, psswd) {
-  	//TODO
+  	//TODO ajax
+  	if(!(uname && psswd)) {
+  		//TODO error code
+  		return "ERROR_CODE";
+  	}
+
+  	//TODO ajax
+  	//Dummy User:
+  	me.uname = "go_dawgs";
+  	me.fname = "Team";
+  	me.lname = "Player"
+  }
+
+  UserModel.createAccount = function(fname, lname, uname, psswd_one, psswd_two) {
+  	if(!(fname && lname && uname && pswd_one && pswd_two)) {
+      
+      //TODO error code
+      return "ERROR_CODE";
+    } else if(pswd_one != pswd_two) {
+    	return "PASSWORD_FAIL";
+    }
+
+    //TODO ajax
   }
 
   return UserModel;
