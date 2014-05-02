2e0c9ad74ec67ca757d38e86665953c9df4a70c1
Panji Wisesa <panjiw@uw.edu>
2014-05-02 14:03:08 -0700

    Prelim login system

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
     self.username = username.downcase
     self.email = email.downcase
   }
+  before_create :create_remember_token
+
   VALID_USERNAME_REGEX = /\A[\w]+\z/i
   validates :username, presence: true, length: { maximum: 255 }, format: { with: VALID_USERNAME_REGEX }, uniqueness: { case_sensitive: false }
   validates :firstname, presence: true, length: { maximum: 255 }
@@ -11,4 +13,17 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  match '/',       to: 'static_pages#index',  via: 'get'
+  match '/',            to: 'static_pages#index',  via: 'get'
   match '/index',       to: 'static_pages#index',  via: 'get'
-  match '/home',   to: 'static_pages#home',   via: 'get'
-  match '/create_user', to: 'users#create', via: 'post'
+  match '/home',        to: 'static_pages#home',   via: 'get'
+  match '/create_user', to: 'users#create',        via: 'post'
+  match '/sign_in',     to: 'sessions#create',     via: 'post'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
