a6060eb8bf6b28b75c32aabab110cdf1be322c1b
Karthik Palaniappan <karth295@cs.uw.edu>
2014-05-01 18:40:55 -0700

    Removed unnecessary files

--- app/assets/javascripts/controllers/loginViewController.js
+++ app/assets/javascripts/controllers/loginViewController.js
@@ -1,8 +1,5 @@
-var myApp = angular.module('myapp');
-myApp.controller("UserController", ["$scope", "UserModel", function($scope, UserModel) {
-	console.log("Hello, scope" + $scope);
-	$scope.message = "Hello, world";
 
+angular.module('myapp').controller("loginViewController", ["$scope", "UserModel", function($scope, UserModel) {
 	$scope.login = function(e) {
 		var response = UserModel.login($scope.login_uname, $scope.login_psswd);
 
@@ -15,13 +12,13 @@ myApp.controller("UserController", ["$scope", "UserModel", function($scope, User
 
 	$scope.createAccount = function(e) {
 		var response = UserModel.createAccount($scope.create_fname, $scope.create_lname, 
-												$scope.create_uname, $scope.create_psswd_one, 
+												$scope.create_uname, $scope.create_email, $scope.create_psswd_one,
 												$scope.create_psswd_two);
 
 		if(response) {
 			//TODO error codes
 		} else {	//Success!
-			$scope.create_fname = $scope.create_lname = $scope.create_uname = $scope.create_psswd_one = $scope.create_psswd_two = "";
+			$scope.create_fname = $scope.create_lname = $scope.create_uname = $scope.create_email = $scope.create_psswd_one = $scope.create_psswd_two = "";
 		}
 	}
 }]);
