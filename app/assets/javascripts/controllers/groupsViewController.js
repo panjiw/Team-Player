c5060205605ba4089d11ed99532bc19042d3aecf
FUNG C <kst10037@gmail.com>
2014-05-03 22:23:52 -0700

    make add group call a function sendForm()

--- app/assets/javascripts/controllers/groupsViewController.js
+++ app/assets/javascripts/controllers/groupsViewController.js
@@ -7,4 +7,11 @@
 angular.module('myapp').controller("groupsViewController", ["$scope", "TaskModel", function($scope, TaskModel) {
 
 
+	// this function is for backend testing, not for release.
+	$scope.sendForm = function(){
+		console.log("keith, send a form!");
+		// do your thing
+
+	}
+
 }]);
