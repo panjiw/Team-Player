e98180fbd76244eff9fdd21b060b82bed7975cf1
MADLinsen <linsenwu@comcast.net>
2014-05-13 16:14:35 -0700

    Changed bill popovers to ng-click

--- app/assets/javascripts/controllers/billsViewController.js
+++ app/assets/javascripts/controllers/billsViewController.js
@@ -8,14 +8,11 @@ angular.module('myapp').controller("billsViewController", ["$scope", "TaskModel"
 	$scope.activeBillTab='bill_selected_you_owe';
 
   $(function () { $("[data-toggle='popover']").popover({ html : true }); });
-  $('.btn').on('click', function (e) {
+  $scope.openPop = function (e) {
     $('.btn').on('click', function (e) {
         $('.btn').not(this).popover('hide');
     });
-  });
-  $('a').on('click', function (e) {
-      $('.btn').popover('hide');
-  });
+  };
 
   $('#openBtn').click(function(){
   	$('#myModal').modal({show:true})
