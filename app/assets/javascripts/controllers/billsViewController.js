4f275f5909eac6d98ac973ae8b6a41f8d5880af8
FUNG C <kst10037@gmail.com>
2014-05-14 00:07:01 -0700

    add bill now have mini calendar.

--- app/assets/javascripts/controllers/billsViewController.js
+++ app/assets/javascripts/controllers/billsViewController.js
@@ -4,18 +4,18 @@
  *  This file is not implemented yet. It will be
  *  the controller for the bills page
  */
-angular.module('myapp').controller("billsViewController", ["$scope", "BillModel", function($scope, BillModel) {
-	$scope.activeBillTab='bill_selected_you_owe';
+angular.module('myapp').controller("billsViewController", ["$scope", "BillModel", "GroupModel", 
+  function($scope, BillModel, GroupModel) {
+  $scope.activeBillTab='bill_selected_you_owe';
+  $scope.newBillTitle = "";
+  $scope.newBillDescription = "";
+  $scope.newBillDateDue = "";
+  $scope.newBillTotal = "";
+  $scope.newBillGroup = "";
+  $scope.newBillMembers = "";
 
-  $scope.createBill = function(e) {
-    // dummy bill data:
-    var groupID = 1;
-    var title = "bill_title";
-    var description = "bill_description!";
-    var dateDue = new Date();
-    var total = 100;
-    var membersAmountMap = {1:25, 2:75};
-    BillModel.createBill(groupID, title, description, dateDue, total, membersAmountMap,
+  $scope.getBillFromModel = function(e) {
+    BillModel.getBillFromServer(
       function(error){
       if(error){
         //TODO
@@ -24,6 +24,24 @@ angular.module('myapp').controller("billsViewController", ["$scope", "BillModel"
       }
     });
   }
+
+  $scope.createBill = function(e) {
+    // dummy bill data:
+    var groupID = 57;
+    var title = "bill_title 3";
+    var description = "bill_description! 3";
+    var dateDue = new Date();
+    var total = 30;
+    var membersAmountMap = {1:4, 3:6, 4:20};
+    BillModel.createBill(groupID, title, description, dateDue, total, membersAmountMap,
+      function(error){
+      if(error){
+        //TODO
+      } else{
+        //TODO
+      }
+    });
+  };
 
   $(function () { $("[data-toggle='popover']").popover({ html : true }); });
   $('.btn').on('click', function (e) {
@@ -36,7 +54,7 @@ angular.module('myapp').controller("billsViewController", ["$scope", "BillModel"
   });
 
   $('#openBtn').click(function(){
-  	$('#myModal').modal({show:true})
+    $('#myModal').modal({show:true})
   });
 
 }]);
