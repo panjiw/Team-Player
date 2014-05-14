0a831f90a192bf49686905cc8dc51453a90d6d0e
MADLinsen <linsenwu@comcast.net>
2014-05-14 11:02:45 -0700

    Changed bills to ng-repeat

--- app/assets/javascripts/controllers/billsViewController.js
+++ app/assets/javascripts/controllers/billsViewController.js
@@ -43,6 +43,34 @@ angular.module('myapp').controller("billsViewController", ["$scope", "BillModel"
     });
   };
 
+  $scope.billsYouOweMap = [];
+
+  $scope.billsYouOwe = [
+    {person:'Member 1', amount: 12, why: 'Bought Lunch'},
+    {person:'Member 1', amount: 68, why: 'Paid Electric Bill'},
+    {person:'Member 3', amount: 32, why: 'Bought Toilet Paper'},
+    {person:'Member 1', amount: 44, why: 'Paid Internet Bill'},
+    {person:'Member 2', amount: 23, why: 'Bought Lunch'},
+    {person:'Member 1', amount: 8, why: 'Bought Dinner'}];
+    
+  $(function () {
+    $.each($scope.billsYouOwe, function(bill) {
+      var found = false;
+      var bill = this;
+      $.each($scope.billsYouOweMap, function(member) {
+        if (this.person == bill.person) {
+          this.amount += bill.amount;
+          this.bills.push({amt: bill.amount, why: bill.why});
+          found = true;
+          return false;
+        }
+      });
+      if (!found) {
+        $scope.billsYouOweMap.push({person: bill.person, amount: bill.amount, bills: [{amt: bill.amount, why: bill.why}]});
+      }
+    });
+  });
+
   $(function () { $("[data-toggle='popover']").popover({ html : true }); });
   $scope.openPop = function (e) {
     $('.btn').on('click', function (e) {
