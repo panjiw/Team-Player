e31b9c95325abb2241fa5749f11d11451aafeaed
MADLinsen <linsenwu@comcast.net>
2014-05-14 11:11:01 -0700

    Bills owed to you also ng-repeat

--- app/assets/javascripts/controllers/billsViewController.js
+++ app/assets/javascripts/controllers/billsViewController.js
@@ -84,6 +84,34 @@ angular.module('myapp').controller("billsViewController", ["$scope", "BillModel"
       }
     });
   });
+  
+  $scope.billsOweYouMap = [];
+
+  $scope.billsOweYou = [
+    {person:'Member 1', amount: 12, why: 'Bought Lunch'},
+    {person:'Member 2', amount: 68, why: 'Paid Electric Bill'},
+    {person:'Member 3', amount: 32, why: 'Bought Toilet Paper'},
+    {person:'Member 4', amount: 44, why: 'Paid Internet Bill'},
+    {person:'Member 2', amount: 23, why: 'Bought Lunch'},
+    {person:'Member 1', amount: 8, why: 'Bought Dinner'}];
+    
+  $(function () {
+    $.each($scope.billsOweYou, function(bill) {
+      var found = false;
+      var bill = this;
+      $.each($scope.billsOweYouMap, function(member) {
+        if (this.person == bill.person) {
+          this.amount += bill.amount;
+          this.bills.push({amt: bill.amount, why: bill.why});
+          found = true;
+          return false;
+        }
+      });
+      if (!found) {
+        $scope.billsOweYouMap.push({person: bill.person, amount: bill.amount, bills: [{amt: bill.amount, why: bill.why}]});
+      }
+    });
+  });
 
   $(function () { $("[data-toggle='popover']").popover({ html : true }); });
   $scope.openPop = function (e) {
