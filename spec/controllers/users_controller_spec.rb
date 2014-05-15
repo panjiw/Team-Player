4b1d6e15bd004f201d790a5ecb562bdc4862a7df
Micaela R. Tolliver <mtolliver42@gmail.com>
2014-05-14 18:11:59 -0700

    Added User Controller Tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,7 +9,6 @@ require 'spec_helper'
 describe UsersController do
 #itegrate_views
 
-
 # tests if creates a valid user
 describe "GET show" do
     before(:each) do
@@ -18,7 +17,6 @@ describe "GET show" do
     end
 
     context 'creating one user' do
-    	puts 'here'
     	it 'should add one user' do
 			@user.should be_valid
 		end
@@ -26,22 +24,104 @@ describe "GET show" do
 
 end
 	
-
-# tests than, when attempting to create a user with an invalid password, throws an error
-describe "adding incorrect user" do
-	context 'creating one user' do
-    	it 'should not add one user' do
+# tests than, when attempting to create an invalid user, throws an error
+describe "adding an incorrect user" do
+	
+    context 'creating mismatching passwords user' do
+    	it 'should throw an error' do
     		lambda { User.create! :username => "teamplayer", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
                               :password => "player", :password_confirmation => "notworking" }.should raise_error
 		end
 	end	
 
+    context 'creating too short password user' do
+        it 'should throw an error' do
+            lambda { User.create! :username => "teamplayer", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+                              :password => "no", :password_confirmation => "no" }.should raise_error
+        end
+    end 
+
+    context 'email already in database' do
+        it 'should throw an error' do
+        @user = User.create! :username => "teamplayer", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+                            :password => "player", :password_confirmation => "player"
+        lambda { User.create! :username => "newname", :firstname => "New", :lastname => "Name", :email => "team@player.com",
+                            :password => "player", :password_confirmation => "player"}.should raise_error
+        end
+    end
+
+    context 'username already in database' do
+        it 'should throw an error' do
+        @user = User.create! :username => "teamplayer", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+                            :password => "player", :password_confirmation => "player"
+        lambda { User.create! :username => "teamplayer", :firstname => "New", :lastname => "Name", :email => "new@email.com",
+                            :password => "player", :password_confirmation => "player"}.should raise_error
+        end
+    end
+
 end
-	
 
+# tests the controller's create method
+describe "CREATE new" do
+    before(:each) do
 
+        @user = User.create! :username => "teamplayer", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+                              :password => "player", :password_confirmation => "player"
+        
+    end
 
+    # tests when every value is correct that create works
+    context 'should create user' do
+        it 'should be ok ' do
+            post 'create', :user => {:username => "newusername", :firstname => "Team", :lastname => "Player", :email => "new@player.com",
+                             :password => "player", :password_confirmation => "player"}
+           #response.body.should_be_valid token:
+            (response.body.include? "token").should be_true
+            #value.should_be true
+        end
+    end
 
+    # test when username is already in database that create does not work
+    context 'should not create user' do
+        it 'should not be ok ' do
+            post 'create', :user => {:username => "teamplayer", :firstname => "Team", :lastname => "Player", :email => "no@player.com",
+                             :password => "player", :password_confirmation => "player"}
+            (response.body.include? "error").should be_true
+            #(respone.body.include? "Username has already been taken").should be_true
+        end 
+    end
+
+    # tests when email is already in database that create does not work
+    context 'should not create user' do
+        it 'should not be ok ' do
+            post 'create', :user => {:username => "tp", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+                             :password => "player", :password_confirmation => "player"}
+            (response.body.include? "error").should be_true
+            #(respone.body.include? "Email has already been taken").should be_true
+        end 
+    end
+
+    # tests when password size is not long enough that create does not work
+    context 'should not create user' do
+        it 'should not be ok ' do
+            post 'create', :user => {:username => "tp", :firstname => "Team", :lastname => "Player", :email => "new@player.com",
+                             :password => "play", :password_confirmation => "play"}
+            (response.body.include? "error").should be_true
+            #(respone.body.include? "Email has already been taken").should be_true
+        end 
+    end
+
+    # tests when passwords do not match create does not work
+    context 'should not create user' do
+        it 'should not be ok ' do
+            post 'create', :user => {:username => "tp", :firstname => "Team", :lastname => "Player", :email => "new@player.com",
+                             :password => "playrrrrrrr", :password_confirmation => "playeeeee"}
+            (response.body.include? "error").should be_true
+            #(respone.body.include? "Email has already been taken").should be_true
+        end 
+    end
+
+end
 
 # # 
 # before do
