ab253c7336af6218bbd67c3bd585cb48aad3cf4a
Micaela R. Tolliver <mtolliver42@gmail.com>
2014-05-15 17:33:37 -0700

    Created Finduseremail Tests; Updated Routes
    Created tests for finduseremail in users_controller spec.
    Cleaned up users_controller test and sessions_controller spec
    Created bills_controller with tests for GET; no tests for NEW yet.
    Updated routes to route to the correct bills method.
    Updated update in users_controller

--- spec/controllers/bills_controller_spec.rb
+++ spec/controllers/bills_controller_spec.rb
@@ -7,10 +7,95 @@
 require 'spec_helper'
 
 describe BillsController do
-#itegrate_views
+
+  # match '/create_bill', to: 'bills#new', via: 'post'
+  # match '/get_bills', to: 'bills#get_all',   via: 'get'
 
 #tests for new
+describe "testing NEW" do
+
+	before(:each) do
+		@controller = UsersController.new
+		post 'create', :user => {:username => "takenname", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+        	:password => "player", :password_confirmation => "player"}
+        @controller = BillsController.new
+		end
+
+	# tests creating a bill when user isn't logged in
+	context 'user is not signed in' do
+		# it 'should not show bills' do
+		# 	post 'new'
+		# 	(response.body.include? "redirected").should be_true
+		# 	end
+		end
+
+	# test creating a split bill with one user
+	context 'the user creates a bill with one user' do
+		
+		before(:each) do
+			@controller = SessionsController.new
+        	post 'create', :user => {:username => "takenname", :password => "player"}
+        	@controller = BillsController.new
+			end
+
+		# group_id: params[:bill][:group_id],
+  #                      user_id: view_context.current_user[:id],
+  #                      title: params[:bill][:title],
+  #                      description: params[:bill][:description],
+  #                      due_date: params[:bill][:due_date],
+  #                      total_due: params[:bill][:total_due])
+
+
+		it 'should split correctly' do
+			# post 'new', :bill => {:title => "title", :description => "description", :due_date => "date?", :total_due => 10, :group_id => 1, :members => [1]}
+
+
+
+			end
+	end
+
+
+	# tests creating a split bill with uneven split
+
+	# tests creating a bill when prices do not add up
+
+	# tests creating a bill when prices add up and only one person
+
+	# tests creating a bill when prices add up and more than one person
+
+	# tests if bill with multiple people with correct total and splitting is added
+
+end
 
 #tests for getbill
+describe "testing getbill" do	
+	# tests what is returned when the user isn't signed in
+	context 'user is not signed in' do
+		it 'should not show bills' do
+			get 'get_bills'
+			(response.body.include? "redirected").should be_true
+		end
+	end
+
+	# tests what is returned when user is not signed in
+	context 'user is signed in' do
+		it 'should show ok json query' do
+			@controller = UsersController.new
+			post 'create', :user => {:username => "takenname", :firstname => "Team", :lastname => "Player", :email => "team@player.com",
+        		 :password => "player", :password_confirmation => "player"}
+            
+        	@controller = SessionsController.new
+        	post 'create', :user => {:username => "takenname", :password => "player"}
+	
+        	@controller = BillsController.new
+
+        	get 'get_bills'
+        	(response.body.include? "bill").should be_true
+        	(response.body.include? "OK").should be_true
+		end
+	end
+
+end
+
 
 end
