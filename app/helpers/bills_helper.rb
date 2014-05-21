b8fcc7fba00490569f52d9a9a86ccdec59c595de
kyeungk <kyeungk@uw.edu>
2014-05-20 23:24:27 -0700

    removed useless else check

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -4,7 +4,7 @@ module BillsHelper
       if (b.user_id = current_user.id && b.group_id = group.id)
         # user is creator of the bill, destroy the bills 
         b.destroy
-      else (b.group_id = group.id)
+      else
         amt = b.bill_actors.where(user_id: current_user.id).first.due
         b.total_due = b.total_due - amt
         b.users.delete(current_user)
