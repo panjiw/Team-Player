d1ab597c82e589a3595087aa3ce872f0aed98bb8
Karthik Palaniappan <karth295@cs.uw.edu>
2014-05-21 12:45:32 -0700

    Added function to order objects by an array of properties in main.js. It is now being used in the groups page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,3 +6,39 @@
  */
 
 var myApp = angular.module('myapp', ['ui.calendar', 'ui.sortable']);
+
+function compareProperties(a, b){
+    if(a == b) {
+    	return 0;
+    } else if(a < b) {
+    	return -1;
+    } else {
+    	return 1;
+    }
+}
+
+myApp.filter('orderObjectByArray', function(){
+ return function(input, attributes) {
+    if (!angular.isObject(input)) return input;
+
+    var array = [];
+    for(var objectKey in input) {
+        array.push(input[objectKey]);
+    }
+
+    array.sort(function(a, b) {
+    	var compare = 0;
+    	for(var i = 0; i < attributes.length && compare == 0; i++) {
+    		console.log("Looking at: ", attributes[i]);
+    		if(attributes[i][0] == "!") {
+    			var attrib = attributes[i].substring(1);
+    			compare = compareProperties(b[attrib], a[attrib]);
+    		} else {
+    			compare = compareProperties(a[attributes[i]], b[attributes[i]]);
+    		}
+    	}
+    	return compare;
+    });
+    return array;
+ }
+});
