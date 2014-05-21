0ed2690695259ac78cf2f9e75c92284c8000ed42
kyeungk <kyeungk@uw.edu>
2014-05-20 23:26:00 -0700

    fixed if statement already checked by where clause

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,7 +1,7 @@
 module BillsHelper
    def remove_user(group)
     current_user.bills.where(group_id: group.id).each do |b|
-      if (b.user_id = current_user.id && b.group_id = group.id)
+      if (b.user_id = current_user.id)
         # user is creator of the bill, destroy the bills 
         b.destroy
       else
