a1d4937785af70c14b1e41370e5e41132fee68df
Karthik Palaniappan <karth295@cs.uw.edu>
2014-05-23 15:47:11 -0700

    Removed the excessive .'s from group model

--- app/assets/javascripts/controllers/groupsViewController.js
+++ app/assets/javascripts/controllers/groupsViewController.js
@@ -15,54 +15,7 @@
   $scope.groupsList = GroupModel.groups;
   $scope.currentUser = UserModel.get(UserModel.me);
 
-  // function getGroupsFromModel(callback) {
-  //   GroupModel.getGroups(function(groups, asynch, error) {
-  //     if (error){
-  //       console.log("fetch group error:");
-  //       console.log(error);
-  //     } else {
-  //       function groupsToApply() {
-  //         console.log("Got groups:");
-  //         console.log(groups);
-  //         $scope.groupsList = groups;
-  //       }
-  //       if(asynch || (!$scope.$$phase && !$scope.$root.$$phase)) {
-  //         $scope.$apply(groupsToApply);
-  //       } else {
-  //         groupsToApply();
-  //       }
-  //     }
-  //     if(callback) {
-  //       callback();
-  //     }
-  //   });
-  // }
   $scope.$watch('groupsList', function(newVal, oldVal) {});
-
-  function updateGroups() {
-    getGroupsFromModel(function() {
-      function getGroups(){
-        if(Object.getOwnPropertyNames($scope.groupsList).length != 0){
-          $scope.group_selected = Object.keys($scope.groupsList)[0];
-          console.log("$scope.groupsList", $scope.groupsList);
-          console.log("$scope.group_selected", $scope.group_selected);
-          if($scope.group_selected != -1) {
-            console.log("member selected",$scope.member_selected);
-            $scope.currentMembers = $scope.groupsList[$scope.group_selected].members;    
-            $scope.currentMembers = buildMemberMap($scope.currentMembers);
-            console.log("$scope.currentMembers",$scope.currentMembers);
-          }
-        }
-      }
-
-      if(!$scope.$$phase && !$scope.$root.$$phase) {
-        $scope.$apply(getGroups);
-      } else {
-        getGroups();
-      }
-    });
-  }
-  updateGroups();
 
   function buildMemberMap(memberArray){
     var map = {};
@@ -80,14 +33,6 @@
     $('#groupModal').modal({show:true});
     $('.modal-content').css('height',$( window ).height()*0.8);
   }
-
-  // experimental code; when the second line of
-  // this function gets called, the view will be updated.
-  // the second line is required, since it updates the $scope variable.
-  // $scope.change = function(e) {
-  // GroupModel.groups[3] = new Group(2, false, "geo", "geo post description", 0, new Date(), [0]);
-  // $scope.groupsList= GroupModel.getGroups();
-  // }
 
   $scope.createGroup = function(e) {
     if(!$scope.groupCreateName || !$scope.groupCreateDescription) {
@@ -112,7 +57,6 @@
       if (error){
         toastr.error(error);
       } else {
-        getGroupsFromModel();
         $scope.$apply(function() {
           $scope.groupCreateName = $scope.groupCreateDescription = $scope.newMember = "";
           $scope.newMemberList = [$scope.currentUser];
@@ -134,12 +78,10 @@
       if (error){
         toastr.error(error);
       } else {
-        getGroupsFromModel(function() {
-          $scope.$apply(function() {
-            $scope.currentMembers = buildMemberMap($scope.groupsList[groupid].members);
-            $scope.member_selected = -1;
-            $scope.addNewMember = "";
-          });
+        $scope.$apply(function() {
+          $scope.currentMembers = buildMemberMap($scope.groupsList[groupid].members);
+          $scope.member_selected = -1;
+          $scope.addNewMember = "";
         });
       }
     });
@@ -156,8 +98,6 @@
     GroupModel.editGroup(id, $scope.editName, $scope.editDescription, function(error) {
       if(error) {
         toastr.error(error);
-      } else {
-        getGroupsFromModel();
       }
       $('#editModal').modal('hide');
     });
@@ -223,6 +163,4 @@
     $('#'+'groupHelp'+pageNum).hide();
     $('#'+'groupHelp'+(parseInt(pageNum)-1)).show();
   }
-
-
 }]);
