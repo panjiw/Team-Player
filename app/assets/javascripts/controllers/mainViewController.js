757a0079cfabc9eeaba85d56ddd4d674fd1bfa75
FUNG C <kst10037@gmail.com>
2014-05-23 22:30:35 -0700

    documented and comment on main view controller

--- app/assets/javascripts/controllers/mainViewController.js
+++ app/assets/javascripts/controllers/mainViewController.js
@@ -6,62 +6,38 @@
  */
 angular.module('myapp').controller("mainViewController", ["$scope", "UserModel", "GroupModel", 
   function($scope, UserModel, GroupModel) {
-	$scope.activeTab = "selected_home";
-    $scope.activeBillTab = "bill_selected_you_owe";
-    $scope.currentUser = {};
 
+  // initialize tab to home, and initialize bill tab to "you owe" view
+	$scope.activeTab = "selected_home";
+  $scope.activeBillTab = "bill_selected_you_owe";
+  $scope.currentUser = {};
+
+  // get the current logged on user from server
   UserModel.fetchUserFromServer(function(error){
   	if(error){
-  		//TODO
   	} else {
   		$scope.$apply(function(){
   			$scope.currentUser = UserModel.users[UserModel.me];
   		})
   		console.log("fetch user success!");
-
   	}
   });
 
-  // GroupModel.getGroups(function(groups, asynch, error) {
-  //     if (error){
-  //       console.log("fetch group error:");
-  //       console.log(error);
-  //     } else {
-  //       function groupsToApply() {
-  //         console.log("Got groups:");
-  //         console.log(groups);
-  //         $scope.groupsList = groups;
-  //       }
-  //       if(asynch) {
-  //         $scope.$apply(groupsToApply);
-  //       } else {
-  //         groupsToApply();
-  //       }
-  //     }
-  //   });
-
-  $scope.$watch('currentUser', function(newVal, oldVal){
-    console.log('currentUser changed');
-  });
-
-  $scope.printUser = function(){
-    console.log("Users: ",UserModel.users);
-  }
+  // use watch to update the view on username
+  $scope.$watch('currentUser', function(newVal, oldVal){});
 
 	// Log out the user, or display why it failed
 	$scope.logout = function(e) {
 		UserModel.logout(function(error) {
 			if(error) {
-				//TODO deal with errors
-				alert(error);
+				// display every error
+        for (var index in error){
+          toastr.error(error[index]);  
+        }
 			} else {
 				// Log out success, do nothing.
 			}
 		});
 	}
-  
-  $scope.openPop = function (e) {
-        $('.btn').not(e.target).popover('hide');
-  };
 
 }]);
